ed6475151b37f6125eb7a8330ef17474aea10c1d
Santos Odigie <santosodigie104@gmail.com>
2024-10-19 23:34:56 +0100

    added items to list

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, This is a simple project to help decide what show I should watch");
+
+Random Rand = new Random();
+list_of_shows = ["Code Geass", "One Piece, Fairy Tale", "My Hero Academia", "Demon Slayer"];
